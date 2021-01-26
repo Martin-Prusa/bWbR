ff7f8d0a25972555726b15a0f3ef14c8bb0ffefc
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2021-01-26 12:25:17 +0100

    cisla

---
+++ src/com/company/Main3.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main3 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("in.txt"));
+            int cislo = 0;
+            String nextLine=br.readLine();
+            while(nextLine!=null) {
+                String[] a = nextLine.split(" ");
+                if(a[0].equals("P")) {
+                    cislo += Integer.parseInt(a[1]);
+                } else {
+                    cislo -= Integer.parseInt(a[1]);
+                }
+                nextLine = br.readLine();
+            }
+            System.out.println(cislo);
+
+        } catch (IOException e) {
+            System.out.println("Nelze přečíst soubor.");
+        }
+    }
+}
