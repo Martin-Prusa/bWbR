2662306cfd11a9e9ae68908747081eebef0b79ed
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2021-01-26 13:23:49 +0100

    konec

--- src/com/company/Main4.java
+++ src/com/company/Main4.java
@@ -3,49 +3,35 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main4 {
     public static void main(String[] args) {
-        System.out.println(decode("ahoj"));
-        System.out.println(encode("heud"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1)Zašifrování \n2)Odšífrování");
+        int volba = Integer.parseInt(sc.nextLine());
+        if(volba == 1) {
+            System.out.println("Zadej slovo k zašifrování: ");
+            System.out.println("Zašifrované slovo: "+code(sc.nextLine(), 'd'));
+        } else {
+            System.out.println("Zadej slovo k rozšifrování: ");
+            System.out.println("Rozšifrované slovo: "+code(sc.nextLine(), 'e'));
+        }
     }
 
-    public static String decode(String str) {
+    public static String code(String str, char ch) {
         String newStr = str;
         try {
             BufferedReader br = new BufferedReader(new FileReader("cypher.txt"));
             String nextLine = br.readLine();
             while(nextLine != null) {
                 String[] a = nextLine.split(" ");
-                char one = (char) Integer.parseInt(a[0], 2);
-                char two = (char) Integer.parseInt(a[1], 2);
+                char one = ch=='d'?(char) Integer.parseInt(a[0], 2) : (char) Integer.parseInt(a[1], 2);
+                char two = ch=='e'?(char) Integer.parseInt(a[0], 2) : (char) Integer.parseInt(a[1], 2);
                 String xx = ""+one;
                 if(str.contains(xx)) {
                     int x = str.indexOf(one);
                     newStr = newStr.substring(0, x)+two+newStr.substring(x+1);
-                }
-                nextLine = br.readLine();
-            }
-            br.close();
-        } catch (IOException e) {
-            System.out.println("Nelze přečíst soubor");
-        }
-        return newStr;
-    }
-
-    public static String encode(String str) {
-        String newStr = str;
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("cypher.txt"));
-            String nextLine = br.readLine();
-            while(nextLine != null) {
-                String[] a = nextLine.split(" ");
-                char one = (char) Integer.parseInt(a[0], 2);
-                char two = (char) Integer.parseInt(a[1], 2);
-                String xx = ""+two;
-                if(str.contains(xx)) {
-                    int x = str.indexOf(two);
-                    newStr = newStr.substring(0, x)+one+newStr.substring(x+1);
                 }
                 nextLine = br.readLine();
             }
